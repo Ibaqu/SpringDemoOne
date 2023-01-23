fc4f9ae39faa08caddd67a3fecb2f9348234fcae
Ibaqu <aquib.zt@gmail.com>
2023-01-22 20:28:00 -0500

    Demo Injecting Literal Values

--- src/com/ibaqu/springdemo/CricketCoach.java
+++ src/com/ibaqu/springdemo/CricketCoach.java
@@ -5,14 +5,37 @@ public class CricketCoach implements Coach {
 	// Dependency definition
 	private FortuneService fortuneService;
 	
+	// add new fields for email add and team
+	private String emailAddress;
+	
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		System.out.println("CricketCoach : inside setter method - setEmailAddress");
+		this.emailAddress = emailAddress;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		System.out.println("CricketCoach : inside setter method - setTeam");
+		this.team = team;
+	}
+
+	private String team;
+	
 	// Create constructor
 	public CricketCoach() {
-		System.out.println("CricketCoack : inside no-arg constructor");
+		System.out.println("CricketCoach : inside no-arg constructor");
 	}
 	
 	// Setter method called by spring
 	public void setFortuneService(FortuneService fortuneService) {
-		System.out.println("CricketCoack : inside setter method - setFortuneService");
+		System.out.println("CricketCoach : inside setter method - setFortuneService");
 		this.fortuneService = fortuneService;
 	}
 
